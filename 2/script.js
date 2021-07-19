17650138703f2fa621a652792dc1f54a5644915c
kirill1991okt <kirill1991okt@gmail.com>
2021-07-19 14:58:06 +0300

    add: solve task 4 lesson 2

--- 2/script.js
+++ 2/script.js
@@ -36,9 +36,53 @@ if(login === "Админ"){
 }
 
 
+// Задача 3
+
+var message;
+
+(login === 'Вася') ? message = 'Привет' :
+(login === 'Директор') ? message = 'Здравствуйте' : 
+(login === '') ? message  = 'Нет логина' : message = '';
 
 
 
+// Задание 4
 
+var firstName = prompt('Ваша фамилия');
+
+while (firstName === ''){
+    alert ('Ошибка, введите корректные данные')
+    var firstName = prompt('Ваша фамилия');
+}
+
+if(firstName === null) {
+    firstName = '-';
+}  
+
+var name = prompt('Ваше имя');
+var patronymic = prompt('Ваше отчество');
+
+
+
+var ageInYear = parseInt(prompt('Возраст в годах'));
+var ageInDay = ageInYear*365;
+var ageFromFiveYear = ageInYear + 5;
+
+// var askGender = confirm('Ваш пол - мужской?');
+
+var gender = (confirm('Ваш пол - мужской?')) ? 'мужской' : 'женский'
+
+
+
+var retired = (ageInYear > 62.5 && gender === 'мужской') ? 'да' : 
+              (ageInYear > 57.5 && gender === 'женский') ? 'да' : 'нет';
+
+
+alert ('"Ваше ФИО: ' + firstName + ' ' + name + ' ' + patronymic + ' \n ' + 
+    'Ваш возраст в годах: ' + ageInYear + ' \n ' +
+    'Ваш возраст в днях: ' + ageInDay + ' \n ' +
+    'Через 5 лет вам будет: ' + ageFromFiveYear + ' \n ' + 
+    'Ваш пол: ' + gender + ' \n ' + 
+    'Вы на пенсии: ' + retired + '"');
 
 
