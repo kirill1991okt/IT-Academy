fed9f05900c1c40503d234cb5cf6c699afd4b00e
kirill1991okt <kirill1991okt@gmail.com>
2021-07-26 12:14:37 +0300

    fix: task 4 lesson 3

--- 3/script3.js
+++ 3/script3.js
@@ -2,7 +2,7 @@
 
 var styles = ["Джаз", "Блюз"];
 
-styles.push('Рок-н-Ролл');
+styles.push("Рок-н-Ролл");
 
 styles[styles.length - 2] = "Классика";
 
@@ -10,76 +10,57 @@ alert(styles.shift());
 
 styles.unshift("Рэп", "Регги");
 
-
 // Задание 2
 
 // Вариант 1
 
 for (var i = 2; i <= 10; i++) {
-    if (i % 2 != 0) continue;
-    alert(i);
+  if (i % 2 != 0) continue;
+  alert(i);
 }
 
 for (var i = 2; i <= 10; i++) {
-    if (!(i % 2)) {
-        alert(i);
-    }
+  if (!(i % 2)) {
+    alert(i);
+  }
 }
 
 // Вариант 2
 
 for (var i = 2; i <= 10; i += 2) {
-    alert(i);
+  alert(i);
 } // не уверен, что так можно, технически задание  выполняется
 
-
 // Задание 3
-
 
 var arr = [];
 
 var sum = 0;
 
-
 while (true) {
-
-    var num = prompt('Введите число');
-    if (!num || isNaN(+num)) break;
-    arr[arr.length] = +num;
-
+  var num = prompt("Введите число");
+  if (!num || isNaN(+num)) break;
+  arr[arr.length] = +num;
 }
 
 for (var i = 0; i < arr.length; i++) {
-    sum += arr[i];
+  sum += arr[i];
 }
 
 Alert(sum);
 
-
 // Задание 4
 
-var array = [3,4,5,6,7,8,9,10];
+var array = [3, 4, 5, 6, 7, 8, 9, 10];
 
-for (var i = 0; i < array.length; i++){
-    // if (array[i] === 2) {
-    //     console.log(array[i]);
-    // }
-    // console.log('Здесь арай ' + array[i]);
-    for (var j = 2; j < array[i]; j++){
+for (var i = 0; i < array.length; i++) {
+  if (array[i] === 2) {
+    console.log(array[i]);
+  }
 
-        if(array[i] % j == 0){
-            console.log ('hello' + array[i]);
-        }
-        // console.log('Здесь арай1 ' + array[i]);
-        // console.log (j)
-
-        // if (array[i] % j != 0) {  
-
-        //      console.log ( 'а тут что ' + array[i])
-            
-        //     } 
-
-    } 
-   
+  for (var j = 2; j < array[i]; j++) {
+    if (array[i] % j !== 0) {
+      console.log(j)
+    }
+  }
 }
-
