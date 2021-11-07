808c87281f751af0c6ba4d753c694d2a9d020cda
Kirill <kirill1991okt@gmail.com>
2021-11-07 17:37:58 +0300

    add: lesson 24

---
+++ 24 Node.js/Example/frontend/js/models/tasks.js
@@ -0,0 +1,53 @@
+class Tasks {
+    getTasksList() {
+    	return new Promise(resolve => {
+			const xhr = new XMLHttpRequest();
+
+			xhr.open('GET', 'http://localhost:3000/api/tasks');
+
+			xhr.onload = () => resolve(JSON.parse(xhr.response));
+
+			xhr.send();
+		});
+    }
+
+    addTask(newTask) {
+		return new Promise(resolve => {
+			const xhr = new XMLHttpRequest();
+
+			xhr.open('POST', 'http://localhost:3000/api/task');
+			xhr.setRequestHeader('Content-Type', 'application/json');
+
+			xhr.onload = () => resolve(JSON.parse(xhr.response));
+
+			xhr.send(JSON.stringify(newTask));
+		});
+	}
+
+	getTask(id) {
+		return new Promise(resolve => {
+			const xhr = new XMLHttpRequest();
+
+			xhr.open('GET', `http://localhost:3000/api/task/${id}`);
+
+			xhr.onload = () => resolve(JSON.parse(xhr.response));
+
+			xhr.send();
+		});
+	}
+
+	editTask(updatedTask) {
+		return new Promise(resolve => {
+			const xhr = new XMLHttpRequest();
+
+			xhr.open('PUT', `http://localhost:3000/api/task/${updatedTask.id}`);
+			xhr.setRequestHeader('Content-Type', 'application/json');
+
+			xhr.onload = () => resolve();
+
+			xhr.send(JSON.stringify(updatedTask));
+		});
+	}
+}
+
+export default Tasks;
